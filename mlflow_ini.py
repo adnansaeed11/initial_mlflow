471d79d66c84c8c6c211a231dd8ab7b868767626
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2025-04-30 00:00:27 +0500

    change the author name

--- mlflow_ini.py
+++ mlflow_ini.py
@@ -59,7 +59,7 @@ with mlflow.start_run():
 
     mlflow.sklearn.log_model(dt, "decision tree")
 
-    mlflow.set_tag('author','nitish')
+    mlflow.set_tag('author','Adnan Saeed')
     mlflow.set_tag('model','decision tree')
 
     print('accuracy', accuracy)
