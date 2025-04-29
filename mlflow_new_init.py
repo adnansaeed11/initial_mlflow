d76d9f200a55de5d05a281b5e13fbc27bf39ed87
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2025-04-30 00:29:38 +0500

    just change the author name

--- mlflow_new_init.py
+++ mlflow_new_init.py
@@ -60,7 +60,7 @@ with mlflow.start_run():
 
     mlflow.sklearn.log_model(rf, "random forest")
 
-    mlflow.set_tag('author','rahul')
+    mlflow.set_tag('author','Adnan Saeed')
     mlflow.set_tag('model','random forest')
 
     print('accuracy', accuracy)
